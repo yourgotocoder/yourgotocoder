d5ffc0043f03478ff32b54fd6a0bdcd64bea59e9
yourgotocoder <yourgotocoder@gmail.com>
2022-05-02 18:00:29 +0530

    Setting up the nextauth catch all route

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth/next";
+import Providers from "next-auth/providers";
+
+export default NextAuth({
+  session: {
+    jwt: true,
+  },
+  providers: [
+    Providers.Credentials({
+      async authorize(credentials) {},
+    }),
+  ],
+});
