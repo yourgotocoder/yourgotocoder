35f3321f93913cf6d74394308a14187c5215c460
yourgotocoder <yourgotocoder@gmail.com>
2022-08-06 10:06:48 +0530

    Added GitHub profile link button

--- components/Home/MainComponents/HomeNavbarComponent.tsx
+++ components/Home/MainComponents/HomeNavbarComponent.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import styles from "./HomeNavbarComponent.module.css";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+
 
 type Props = {};
 
 const HomeNavbarComponent = (props: Props) => {
     return <nav className={styles["navbar"]}>
         <div>Blog, Contact Me</div>
-        <div>Github Link</div>
+        <div>
+            <a href="https://github.com/yourgotocoder" target="_blank">
+                <Tooltip title="My GitHub profile">
+                    <IconButton color="secondary">
+                        <GitHubIcon />
+                    </IconButton>
+                </Tooltip>
+            </a>
+        </div>
     </nav>;
 };
 
