2c13db5dd68708270d9d94d40ab855d867d2f5ff
yourgotocoder <yourgotocoder@gmail.com>
2022-08-08 14:37:25 +0530

    Working on a social floater kind of thing

---
+++ components/Common/SocialLinks/SocialLinks.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const SocialLinks = (props: Props) => {
+  return (
+    <div>GitHub, LinkedIn, StackOverflow, Twitter, Reddit</div>
+  )
+}
+
+export default SocialLinks
