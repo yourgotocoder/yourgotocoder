94a79e7fd2bb4b8c7437b0d1251b42a18d77793a
yourgotocoder <yourgotocoder@gmail.com>
2022-08-10 10:01:56 +0530

    Working on my profile skills card

--- components/Home/MainComponents/ProfileSkills.tsx
+++ components/Home/MainComponents/ProfileSkills.tsx
@@ -4,7 +4,11 @@ import styles from "./ProfileSkills.module.css";
 type Props = {};
 
 const ProfileSkills = (props: Props) => {
-    return <div className={styles['profile-card']}>ProfileSkills</div>;
+    return (
+        <div className={styles["profile-card"]}>
+            <h2 style={{textAlign: "center", paddingTop: "10px", fontFamily: "fantasy"}}>My skills</h2>
+        </div>
+    );
 };
 
 export default ProfileSkills;
