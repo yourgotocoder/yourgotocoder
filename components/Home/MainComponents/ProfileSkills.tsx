b29557d67e9fc7112e9a05684b8f4f23aaea3019
yourgotocoder <yourgotocoder@gmail.com>
2022-08-10 11:21:29 +0530

    Skills section done with for now, have to decide what skills to show

--- components/Home/MainComponents/ProfileSkills.tsx
+++ components/Home/MainComponents/ProfileSkills.tsx
@@ -1,12 +1,38 @@
 import React from "react";
 import styles from "./ProfileSkills.module.css";
+import Stack from "@mui/material/Stack";
+import SkillComponent, { SkillProps } from "./SkillComponent";
 
 type Props = {};
+
+const skills: SkillProps[] = [
+    { experience: 4, rating: 5, skill: "JavaScript" },
+    { experience: 3, rating: 5, skill: "TypeScript" },
+    { experience: 4, rating: 4, skill: "Python" },
+];
 
 const ProfileSkills = (props: Props) => {
     return (
         <div className={styles["profile-card"]}>
-            <h2 style={{textAlign: "center", paddingTop: "10px", fontFamily: "fantasy"}}>My skills</h2>
+            <h2
+                style={{
+                    textAlign: "center",
+                    paddingTop: "10px",
+                    fontFamily: "fantasy",
+                }}
+            >
+                My skills
+            </h2>
+            <Stack spacing={1}>
+                {skills.map((skill, index) => (
+                    <SkillComponent
+                        experience={skill.experience}
+                        rating={skill.rating}
+                        skill={skill.skill}
+                        key={index}
+                    />
+                ))}
+            </Stack>
         </div>
     );
 };
---
+++ components/Home/MainComponents/SkillComponent.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styles from "./SkillComponent.module.css";
+import StarsIcon from "@mui/icons-material/Stars";
+
+type RatingRange = 1 | 2 | 3 | 4 | 5;
+
+export type SkillProps = {
+    skill: string;
+    rating: RatingRange;
+    experience: number;
+};
+
+const ratingScale = new Array(5);
+ratingScale.fill(0);
+
+const SkillComponent = (props: SkillProps) => {
+    const { skill, experience, rating } = props;
+    return (
+        <div className={styles["skill-section"]}>
+            <div className={styles["skill"]}>{skill}</div>
+            <div className={styles["skill-rating"]}>
+                {ratingScale.map((element, index) => (
+                    <StarsIcon
+                        sx={{
+                            marginRight: "2",
+                            color: index < rating ? "gold" : "GrayText",
+                        }}
+                        fontSize="inherit"
+                    >
+                    </StarsIcon>
+                ))}
+            </div>
+            <div className={styles["skill-experience"]}>
+                <span className={styles["experience"]}>Experience: </span>{" "}
+                {experience === 1 ? "One year" : `${experience} years`}
+            </div>
+        </div>
+    );
+};
+
+export default SkillComponent;
