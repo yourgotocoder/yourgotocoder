ef90f2dfefcf75646e1a73033fde5fc50ed68a87
yourgotocoder <yourgotocoder@gmail.com>
2022-08-31 10:44:21 +0530

    Added mailto feature for the contact button

--- components/Home/Content/HomeMainContent.tsx
+++ components/Home/Content/HomeMainContent.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import React from "react";
 import FooterComponent from "../../Footer/FooterComponent";
@@ -15,18 +16,37 @@ const HomeMainContent = (props: Props) => {
             component="h1"
             color="dodgerblue"
             className={styles["home-content-header"]}
-            sx={{mb: 1}}
+            sx={{ mb: 1 }}
           >
             Software should be simple. Easy to use.
           </Typography>
 
-          <Typography variant="h5" component="h2" color="GrayText" sx={{mb: 2}} className={styles["home-content-subheader"]}>
+          <Typography
+            variant="h5"
+            component="h2"
+            color="GrayText"
+            sx={{ mb: 2 }}
+            className={styles["home-content-subheader"]}
+          >
             A software developer can build tools to make your life easy. A good
             software engineer can take away the complexity of turning your ideas
             into reality while at the same time giving you a reliable product
             that you know will work everytime, the way you want it to work.{" "}
           </Typography>
-          <Typography variant="h6" component="h3" color="black" className={styles["home-content-about-me"]}>I am Sudarshan Rai, a software developer/engineer with 4 years of real world coding experience</Typography>
+          <Typography
+            variant="h6"
+            component="h3"
+            color="black"
+            className={styles["home-content-about-me"]}
+          >
+            I am Sudarshan Rai, a software developer/engineer with 4 years of
+            real world coding experience
+          </Typography>
+        </div>
+        <div>
+          <a href="mailto:sudarshan@yourgotocoder.com?cc=yourgotocoder@gmail.com" style={{textDecoration: "none"}} target="_blank" rel="noreferrer">
+             <Button variant="contained">Contact</Button>
+          </a>
         </div>
       </main>
     </>
