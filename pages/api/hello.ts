4745c52106040c23e2e9df9550a19d75c0f1c7c0
yourgotocoder <yourgotocoder@gmail.com>
2023-03-31 15:36:24 +0530

    Practicing with astronvim

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    res.status(200).json({ name: 'John Doe' })
+  }
 }
